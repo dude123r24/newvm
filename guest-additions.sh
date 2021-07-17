8a521082f25f89b902d02cae176912bb8be4c0a3
amit.sanghvi <amit2u@hotmail.com>
2021-07-17 14:04:26 +0100

    Amits-MBP16.local

--- guest-additions.sh
+++ guest-additions.sh
@@ -1,7 +1,6 @@
 yum install -y dnf
-dnf install -y gcc automake make bzip2 binutils perl elfutils-libelf-devel 
+dnf install -y gcc automake make bzip2 binutils perl elfutils-libelf-devel dkms patch libgomp glibc-headers glibc-devel kernel-headers
 dnf install -y kernel-uek-devel-$(uname -r)
 dnf install -y dkms patch libgomp glibc-headers glibc-devel kernel-headers
 yum install -y kernel-devel
 yum install -y elfutils*
-
